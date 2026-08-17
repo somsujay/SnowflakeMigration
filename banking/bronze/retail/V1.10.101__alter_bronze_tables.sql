18edc6e7487fcf88a621c2c2494e6a4e85350a11
sujaysom <som.sujay@gmail.com>
2026-08-17 14:29:47 -0400

    Alter Script removed

--- banking/bronze/retail/V1.10.101__alter_bronze_tables.sql
+++
@@ -1,9 +0,0 @@
-/* ============================================================
-   schemachange Migration: V1.1.1__bronze_tables.sql
-   PURPOSE : Bronze layer DDL - raw staging tables
-   ============================================================ */
-
-USE DATABASE {{ database }};
-USE SCHEMA BRONZE;
-ALTER TABLE BRONZE.T_CUSTOMER
-    ADD COLUMN COUNTRY VARCHAR(50);
